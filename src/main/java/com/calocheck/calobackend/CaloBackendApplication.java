ad97b4131f80734be9cf9d2f5a1dd11d0adb682e
neil <naysung03@hanyang.ac.kr>
2025-08-31 19:49:18 +0900

    chore: init backend project

---
+++ src/main/java/com/calocheck/calobackend/CaloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calocheck.calobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaloBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calocheck/calobackend/CaloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calocheck.calobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
